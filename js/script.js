ce7664923d4d40404cf35914fd947a7061be8b08
ElenaS35 <helena0518savitskaya@gmail.com>
2022-07-06 10:59:15 +0200

    commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-  const exampleModal = document.getElementById('exampleModal')
-  exampleModal.addEventListener('show.bs.modal', event => {
-  const button = event.relatedTarget
-  const recipient = button.getAttribute('data-bs-whatever')
-  const modalTitle = exampleModal.querySelector('.modal-title')
-  const modalBodyInput = exampleModal.querySelector('.modal-body input')
-  modalTitle.textContent = `New message to ${recipient}`
-  modalBodyInput.value = recipient
-})
